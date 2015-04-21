3e1fdf7e83074a9af5f88c09c8113f3302e1e13e
foi <foi.first@gmail.com>
2015-04-21 14:59:04 +0700

    теперь веб-сервер отдает информацию

--- linux_agent/src/linux_agent.go
+++ linux_agent/src/linux_agent.go
@@ -2,13 +2,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os/exec"
 	"strconv"
 	"strings"
 	"time"
 )
+
+type AgentData struct {
+	Hostname   string
+	CpuLoad    int64
+	Ram        Ram
+	Swap       Swap
+	Interfaces []NetInterface
+	Disks      []Disk
+}
 
 type Ram struct {
 	TotalRam int64
@@ -40,48 +53,80 @@ func main() {
 	ttt = ttt
 	sss := strings.ToLower("sss")
 	sss = sss
+	http.HandleFunc("/", SendData)
+	err := http.ListenAndServe(":9998", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 	mem := new(Ram)
 	swap := new(Swap)
 	//interfaces := new(NetInterfaces)
-	raw_mem_info, err := ioutil.ReadFile("/proc/meminfo")
+	//raw_mem_info, err := ioutil.ReadFile("/proc/meminfo")
 	if err != nil {
 		fmt.Println(err)
 	}
 	// получаем имя хоста
-	hostname := getHostname()
-	fmt.Println(hostname)
+	//hostname := getHostname()
+	//fmt.Println(hostname)
 	Disks := getPartitionsInfo()
 	fmt.Println(Disks)
 	// считываем информацию об интерфейсах
-	raw_interfaces_stat, err := ioutil.ReadFile("/proc/net/dev")
-	time.Sleep(1 * time.Second)
-	raw_interfaces_stat_after_one_second, err := ioutil.ReadFile("/proc/net/dev")
+	//	raw_interfaces_stat, err := ioutil.ReadFile("/proc/net/dev")
+	//	time.Sleep(1 * time.Second)
+	//	raw_interfaces_stat_after_one_second, err := ioutil.ReadFile("/proc/net/dev")
 
-	parsed_string_ram := parseByteArrayIntoArrayOfString(raw_mem_info)
-	parsed_string_network := parseByteArrayIntoString(raw_interfaces_stat)
-	parsed_string_network_after_one_second := parseByteArrayIntoString(raw_interfaces_stat_after_one_second)
-	normalized_network_string := strings.Split(parsed_string_network, "\n")
+	//parsed_string_ram := parseByteArrayIntoArrayOfString(raw_mem_info)
+	//	parsed_string_network := parseByteArrayIntoString(raw_interfaces_stat)
+	//	parsed_string_network_after_one_second := parseByteArrayIntoString(raw_interfaces_stat_after_one_second)
+	//	normalized_network_string := strings.Split(parsed_string_network, "\n")
 
-	normalized_network_string_after_one_sec := strings.Split(parsed_string_network_after_one_second, "\n")
+	//	normalized_network_string_after_one_sec := strings.Split(parsed_string_network_after_one_second, "\n")
 
-	getMemStat(parsed_string_ram, mem)   // выбираем ОЗУ
-	getSwapStat(parsed_string_ram, swap) // выбираем своп
+	//getMemStat(parsed_string_ram, mem)   // выбираем ОЗУ
+	//getSwapStat(parsed_string_ram, swap) // выбираем своп
 
-	CpuLoad := getCpuUsage()
-	fmt.Println(CpuLoad)
-
-	fmt.Printf("Total RAM %v \n Used Ram %v \n Total Swap %v \n Used Swap %v ",
+	//CpuLoad := getCpuUsage()
+	//fmt.Println(CpuLoad)
+	//var interfacesData []NetInterface
+	//interfacesData = getInterfacesData(interfacesData, normalized_network_string, normalized_network_string_after_one_sec)
+	//fmt.Println(interfacesData)
+	fmt.Printf("Total RAM %v \n Used Ram %v \n Total Swap %v \n Used Swap %v \n Network: %v",
 		mem.TotalRam,
 		mem.UsedRam,
 		swap.TotalSize,
-		swap.CurrentSize)
-	var interfacesData []NetInterface
-	interfacesData = getInterfacesData(interfacesData, normalized_network_string, normalized_network_string_after_one_sec)
-	fmt.Println(interfacesData)
+		swap.CurrentSize,
+		1)
+
 }
 
 // заполним информацию - статистику сетевых интерфейсов
-func getInterfacesData(interfacesData []NetInterface, normalized_network_string []string, normalized_network_string_after_one_sec []string) []NetInterface {
+//func getInterfacesData(interfacesData []NetInterface, normalized_network_string []string, normalized_network_string_after_one_sec []string) []NetInterface {
+
+//	for i := 2; i < len(normalized_network_string)-1; i++ {
+//		str := normalized_network_string[i]
+//		str_after_one_second := normalized_network_string_after_one_sec[i]
+//		arr := strings.Fields(str)
+//		arr_after_one_second := strings.Fields(str_after_one_second)
+//		if arr[0] != "lo:" {
+//			name := arr[0][:len(arr[0])-len(":")]
+//			interfaceData := NetInterface{Name: name,
+//				Guid:          getMACfromNetAdapterName(name),
+//				DownloadSpeed: fromByteToKilobit(fromStringToInt64(arr_after_one_second[1]) - fromStringToInt64(arr[1])),
+//				UploadSpeed:   fromByteToKilobit(fromStringToInt64(arr_after_one_second[9]) - fromStringToInt64(arr[9]))}
+//			interfacesData = append(interfacesData, interfaceData)
+//		}
+//	}
+//	return interfacesData
+//}
+func getInterfacesData() []NetInterface {
+	var interfacesData []NetInterface
+	raw_interfaces_stat, _ := ioutil.ReadFile("/proc/net/dev")
+	time.Sleep(1 * time.Second)
+	raw_interfaces_stat_after_one_second, _ := ioutil.ReadFile("/proc/net/dev")
+	parsed_string_network := parseByteArrayIntoString(raw_interfaces_stat)
+	parsed_string_network_after_one_second := parseByteArrayIntoString(raw_interfaces_stat_after_one_second)
+	normalized_network_string := strings.Split(parsed_string_network, "\n")
+	normalized_network_string_after_one_sec := strings.Split(parsed_string_network_after_one_second, "\n")
 	for i := 2; i < len(normalized_network_string)-1; i++ {
 		str := normalized_network_string[i]
 		str_after_one_second := normalized_network_string_after_one_sec[i]
@@ -100,25 +145,41 @@ func getInterfacesData(interfacesData []NetInterface, normalized_network_string 
 }
 
 // получим информацию об ОЗУ
-func getMemStat(parsed []string, mem *Ram) {
-	total_ram_in_kb, err := strconv.ParseInt(parsed[1], 0, 64)
-	total_available_ram_in_kb, err := strconv.ParseInt(parsed[7], 0, 64)
-	if err != nil {
-		fmt.Println(err)
-	}
+//func getMemStat(parsed []string, mem *Ram) {
+//	total_ram_in_kb, err := strconv.ParseInt(parsed[1], 0, 64)
+//	total_available_ram_in_kb, err := strconv.ParseInt(parsed[7], 0, 64)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	mem.TotalRam = (total_ram_in_kb / 1024)
+//	mem.UsedRam = mem.TotalRam - total_available_ram_in_kb/1024
+//}
+func getMemStat(parsed []string) Ram {
+	mem := Ram{}
+	total_ram_in_kb := fromStringToInt64(parsed[1])
+	total_available_ram_in_kb := fromStringToInt64(parsed[7])
 	mem.TotalRam = (total_ram_in_kb / 1024)
 	mem.UsedRam = mem.TotalRam - total_available_ram_in_kb/1024
+	return mem
 }
 
 // получим информацию о своп
-func getSwapStat(parsed []string, swap *Swap) {
-	total_swap_in_kb, err := strconv.ParseInt(parsed[43], 0, 64)
-	total_free_swap_in_kb, err := strconv.ParseInt(parsed[46], 0, 64)
-	if err != nil {
-		fmt.Println(err)
-	}
+//func getSwapStat(parsed []string, swap *Swap) {
+//	total_swap_in_kb, err := strconv.ParseInt(parsed[43], 0, 64)
+//	total_free_swap_in_kb, err := strconv.ParseInt(parsed[46], 0, 64)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	swap.TotalSize = total_swap_in_kb / 1024
+//	swap.CurrentSize = swap.TotalSize - (total_free_swap_in_kb / 1024)
+//}
+func getSwapStat(parsed []string) Swap {
+	swap := Swap{}
+	total_swap_in_kb := fromStringToInt64(parsed[43])
+	total_free_swap_in_kb := fromStringToInt64(parsed[46])
 	swap.TotalSize = total_swap_in_kb / 1024
 	swap.CurrentSize = swap.TotalSize - (total_free_swap_in_kb / 1024)
+	return swap
 }
 
 // преобразовать массив байт в массив строк
@@ -228,4 +289,29 @@ func getCPUSample() (idle, total uint64) {
 		}
 	}
 	return
+}
+
+// получить данные о своп и РАМ
+func getSwapAndRamData() {
+
+}
+
+// Все что нужно для веб-сервера
+func SendData(w http.ResponseWriter, req *http.Request) {
+	agentData := &AgentData{}
+
+	raw_mem_info, _ := ioutil.ReadFile("/proc/meminfo")
+	parsed_mem_and_swap := parseByteArrayIntoArrayOfString(raw_mem_info)
+	mem := getMemStat(parsed_mem_and_swap)
+	swap := getSwapStat(parsed_mem_and_swap)
+	agentData.Hostname = getHostname()
+	agentData.CpuLoad = getCpuUsage()
+	agentData.Ram.TotalRam = mem.TotalRam
+	agentData.Ram.UsedRam = mem.UsedRam
+	agentData.Swap.TotalSize = swap.TotalSize
+	agentData.Swap.CurrentSize = swap.CurrentSize
+	agentData.Interfaces = getInterfacesData()
+	agentData.Disks = getPartitionsInfo()
+	response, _ := json.Marshal(agentData)
+	io.WriteString(w, string(response))
 }
