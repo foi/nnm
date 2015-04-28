74a2fd98cc611d5d3ca0bd10c54086d1f915df21
foi <foi.first@gmail.com>
2015-04-28 20:09:32 +0700

    исправлен баг с незакрывающимся шелом после пинга

--- ruby-servicennmd/modules/system_ping_helper.rb
+++ ruby-servicennmd/modules/system_ping_helper.rb
@@ -2,14 +2,11 @@ module SystemPingHelper
 
   # ping возвратит количество мс
   def system_ping host, timeout
-    raw = IO.popen "ping -c 1 -W #{timeout} #{host} 2>&1"
-    result = raw.read
-    Process.wait raw.pid
-    if result.size < 30
-      0
-    else
-      splitted_result = result.split(/ /)
-      duration = splitted_result.select {|s| s.include? "time="}[0].delete("time=").to_f.to_i 
+    Open3.popen3("ping", "-c", "1", "-W", timeout.to_s, host.to_s) do |stdin, stdout, stderr, thread|
+      stdin.close
+      raw = stdout.read
+      splitted_result = raw.split(/ /)
+      duration = splitted_result.select {|s| s.include? "time="}[0].delete("time=").to_f.to_i
       duration < 1 ? 1 : duration
     end
   end
